a1a5bbb92dff9c27489f61b7902e8b59d5a4573a
FugaTakata <maple1213goo@gmail.com>
2020-06-04 19:03:14 +0900

    update

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <section className='hero is-danger'>
+        <div className='hero-body'>
+          <div className='container'>
+            <h1 className='title'>react-tutorial in vdslab</h1>
+          </div>
+        </div>
+      </section>
+      <section className='section'>hogehogehogehoge</section>
+      <section className='section'>hogehogehogehoge</section>
+      <footer class='footer'>
+        <div class='content has-text-centered'>
+          <p>
+            &copy; 2020 Fuga Takata
+          </p>
+        </div>
+      </footer>
+    </div>
+  )
+}
+
+export default App
